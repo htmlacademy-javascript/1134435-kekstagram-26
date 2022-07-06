13dc90fc88dc84f756a95eed39152b4cd5f630a2
Keks <keks@htmlacademy.ru>
2022-07-07 00:27:24 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,6 @@
+import './photo.js';
+import { createPost } from './data.js';
+// eslint-disable-next-line no-console
+console.log (
+  createPost()
+);
---
+++ 5/js/util.js
@@ -0,0 +1,38 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (max <= min) {
+    return 'некорректное значение';
+  }
+
+  else {
+    return Math.floor(Math.random() * (max - min + 1)) + min; }
+}
+
+
+//Функция для проверки максимальной длины строки
+function checkLengthComment (line, length) {
+
+  if (line.length > 0 && line.length <= length) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+}
+checkLengthComment ('Hello');
+
+//Функция для рандомного позитивного числа из диапозона
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+
+export { getRandomPositiveInteger,getRandomNumber };
+
